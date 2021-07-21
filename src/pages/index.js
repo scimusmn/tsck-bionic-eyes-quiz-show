d89cc74dd6d9df70881c30e4e4df2629e6155df1
Abdul Rauf <rauf11.abdul@gmail.com>
2021-07-22 03:49:08 +0500

    added language support

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,59 +1,29 @@
 import React from 'react';
 import Home from '@components/Home';
 import { Container } from 'reactstrap';
-import { Link } from 'gatsby';
-import { Global, Attract, Introduction, Score } from '../content';
-import syntaxHighlight from '../helpers/syntaxHighlight';
+import { useTranslation } from 'react-i18next';
+import Layout from '../components/layout';
 
-const IndexPage = () => (
-  <>
-    <Home />
+const IndexPage = () => {
+  const { t, i18n } = useTranslation();
 
-    <Container>
-      <div className='py-5'>
-        <h2>Global Config</h2>
-        <pre>
-          <code
-            dangerouslySetInnerHTML={{
-              __html: syntaxHighlight(JSON.stringify(Global, null, 4)),
-            }}
-          />
-        </pre>
+  function changeLang() {
+    if (i18n.language === 'ar') i18n.changeLanguage('en');
+    else i18n.changeLanguage('ar');
+  }
 
-        <h2 className='mt-5'>Attract Screen</h2>
-        <pre>
-          <code
-            dangerouslySetInnerHTML={{
-              __html: syntaxHighlight(JSON.stringify(Attract, null, 4)),
-            }}
-          />
-        </pre>
+  return (
+    <Layout>
+      <Home />
 
-        <h2 className='mt-5'>Introduction Screen</h2>
-        <pre>
-          <code
-            dangerouslySetInnerHTML={{
-              __html: syntaxHighlight(JSON.stringify(Introduction, null, 4)),
-            }}
-          />
-        </pre>
-
-        <h2 className='mt-5'>Quiz Screen</h2>
-        <p>
-          <Link to='/quiz'>Click here</Link> to view the quiz screen content.
-        </p>
-
-        <h2 className='mt-5'>Score Screen</h2>
-        <pre>
-          <code
-            dangerouslySetInnerHTML={{
-              __html: syntaxHighlight(JSON.stringify(Score, null, 4)),
-            }}
-          />
-        </pre>
-      </div>
-    </Container>
-  </>
-);
+      <Container>
+        <h1>{t('attract.title')}</h1>
+        <button type='button' onClick={changeLang}>
+          Change lang
+        </button>
+      </Container>
+    </Layout>
+  );
+};
 
 export default IndexPage;
