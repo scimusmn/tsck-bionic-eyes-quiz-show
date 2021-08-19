a5d58a70043c2ca8b1971be63f5334660b15585e
Abdul Rauf <rauf11.abdul@gmail.com>
2021-08-19 16:41:09 +0500

    quiz functionality

--- src/components/question/progress.js
+++ src/components/question/progress.js
@@ -2,14 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '@styles/quiz/progress.module.scss';
 
-const Progress = ({ current, total }) => {
-  console.log(current, total);
-  return (
-    <div className={styles.progress}>
-      {current} / {total}
-    </div>
-  );
-};
+const Progress = ({ current, total }) => (
+  <div className={styles.progress}>
+    {current} / {total}
+  </div>
+);
 
 export default Progress;
 
