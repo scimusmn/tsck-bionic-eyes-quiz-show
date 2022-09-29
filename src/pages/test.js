3281edca5d315dead9d862563012c67edd60c720
Caly McMorrow <cmcmorrow@smm.org>
2022-09-29 14:36:51 -0500

    updated captions

---
+++ src/pages/test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const TestPage = () => (
+  <div style={{height: '8000px', padding: '20px', fontSize: '20px', fontFace:'tajawal', direction: 'rtl'}}>
+  	<p style={{paddingBottom: '20px'}}>Test Page</p>
+  	<p style={{paddingBottom: '20px'}}>Q1 Intro text: السؤال الأول</p>
+    <p style={{paddingBottom: '20px'}}>Replaced word in Q5 set 1: ستُنتِج</p>
+    <p style={{paddingBottom: '20px'}}>Full text: يتنبأ الباحثون أنه في خلال سنوات قليلة، ستُنتِج العيون الآلية الحيوية رؤية بجودة هذه الصورة. بل وفي العقود المقبلة، قد تتفوق الرؤية الاصطناعية على الإبصار الطبيعي في القوة والوضوح.</p>
+  	<p>&nbsp;</p>
+  	<p style={{paddingBottom: '20px'}}>Q8-1, replaced word: المُستقبِلات</p>
+  	<p style={{paddingBottom: '20px'}}>المُستقبِلات</p>
+  	<p style={{paddingBottom: '20px'}}>Q2-1, new title: السؤال الأول</p>
+  	<p style={{paddingBottom: '20px'}}>Q2-1, text change: تُعبِّر</p>
+  	<p style={{paddingBottom: '20px'}}>Q 2-2 replaced word, choice 1: سيدة</p>
+  	<p style={{paddingBottom: '20px'}}>Q 2-2 replaced, text answer: تكوِّن </p>
+  	<p style={{padding: '20px'}}>Q 2-5 replaced word: يُطوِّر</p>
+  	<p style={{padding: '20px'}}>Q 2-6 replaced word: الأكِفَّاء</p>
+  	<p style={{padding: '20px'}}>Q 2-8 replaced word: تُقارَن</p>
+  	<p style={{padding: '20px'}}>Q 3-1 new title: السؤال الأول</p>
+  	<p style={{padding: '20px'}}>Q 3-1 text change: المُستخدِم</p>
+  	<p style={{padding: '20px'}}>Q 3-2 text change: تُكوِّن</p>
+  	<p style={{padding: '20px'}}>Q 3-5 text change:للمُستخدِم</p>
+  	<p style={{padding: '20px'}}>Q 3-8 text change:المُستقبِلات</p>
+  </div>
+);
+
+export default TestPage;
